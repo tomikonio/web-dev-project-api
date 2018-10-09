0eaa2c5049794be7c618c96d68df7b4865b549e3
Igor <igrr69@gmail.com>
2018-10-09 10:19:06 +0300

    Complete user profile update

--- server.js
+++ server.js
@@ -37,7 +37,8 @@ app.post("/signin", (req, res) => {
     req.body.email === database.users[0].email &&
     req.body.password === database.users[0].password
   ) {
-    res.json("success");
+    // res.json("success");
+    res.json(database.users[0]);
   } else {
     res.status(400).json("error loggin in");
   }
